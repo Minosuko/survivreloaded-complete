22fc588286aff520e07020b3fe4f14258612f2cd
Minosuko <63941519+Minosuko@users.noreply.github.com>
2023-10-14 20:53:12 +0700

    Create resurviv.js

---
+++ public/js/resurviv.js
@@ -0,0 +1,43 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* new DevMessage("Thanks for playing Resurviv! Here a cookie 🍪", "OK", "test") */
+mainSite = true
+new Activate
+new DevLog
+new UserMessage("Kris Kringle", "Welcome back Kris Kringle!", "Great!", "hasanger")
+new UserMessage("Xu Xialing", "Welcome Xu Xialing!", "OK", "xu")
+new UserMessage("LaVa Ugdado", "No ananas on top of pizza!", "🍍 ➕ 🍕 🟰 💀", "hugo")
+new UserMessage("LaVa ItzChSi", "Uhuuuu lava? 🙃!", "UwU!", "chsi")
+
+
+}
+/*
+     FILE ARCHIVED ON 12:21:54 Apr 18, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 13:46:22 Oct 14, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 135.508
+  exclusion.robots: 0.118
+  exclusion.robots.policy: 0.11
+  cdx.remote: 0.058
+  esindex: 0.008
+  LoadShardBlock: 110.678 (3)
+  PetaboxLoader3.datanode: 95.295 (4)
+  load_resource: 79.054
+  PetaboxLoader3.resolve: 27.478
+*/
