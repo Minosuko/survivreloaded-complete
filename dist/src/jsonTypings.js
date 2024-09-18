2f6132f3e81db32142c59db4658e084aab4a09d1
Minosuko <Catover35390@gmail.com>
2024-09-18 22:37:02 +0700

    delete unused

--- dist/src/jsonTypings.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// todo all the other json files
--- dist/src/utils/vector.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.vAdd = void 0;
-const planck_1 = require("planck");
-function vAdd(a, b) {
-	return (0, planck_1.Vec2)(a.x + b.x, a.y + b.y);
-}
-exports.vAdd = vAdd;
-//# sourceMappingURL=vector.js.map
